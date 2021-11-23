700b1477fec7da82c6fe79773f099a44dda9854a
Kagiso <mphayiw@gmail.com>
2021-11-23 16:16:33 +0200

    first commit

---
+++ myscript.js
@@ -0,0 +1,12 @@
+let sum="";
+function addToSum(operator){
+    sum = sum + operator;
+    document.getElementById("output").innerText =sum;
+}
+function evaluateSum(){
+    document.getElementById("output").innerText = eval(sum);
+}
+function clearSum() {
+    sum = "";
+    document.getElementById("output").innerText = sum;
+}
